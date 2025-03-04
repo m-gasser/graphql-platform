0ce0b57439e57cc560a6766fcbb5b4805ab9716f
Martin Gasser <martin.gasser@digitecgalaxus.ch>
2025-03-04 09:05:00 +0100

    Optimize ClassBaseClassInspector

--- src/HotChocolate/Core/src/Types.Analyzers/Inspectors/ClassBaseClassInspector.cs
+++ src/HotChocolate/Core/src/Types.Analyzers/Inspectors/ClassBaseClassInspector.cs
@@ -19,59 +19,83 @@ public class ClassBaseClassInspector : ISyntaxInspector
         GeneratorSyntaxContext context,
         [NotNullWhen(true)] out SyntaxInfo? syntaxInfo)
     {
-        if (context.Node is ClassDeclarationSyntax { BaseList.Types.Count: > 0, TypeParameterList: null, } possibleType)
+        if (context.Node is not ClassDeclarationSyntax { BaseList: { Types.Count: > 0 } baseList, TypeParameterList: null, } possibleType)
         {
-            var model = context.SemanticModel.GetDeclaredSymbol(possibleType);
-            if (model is { IsAbstract: false, })
+            syntaxInfo = null;
+            return false;
+        }
+
+        var baseTypeSymbols = baseList.Types
+            .Select(t => context.SemanticModel.GetTypeInfo(t.Type).Type)
+            .Where(t => MightBeRelevantType(t))
+            .ToList();
+        if (!baseTypeSymbols.Any())
+        {
+            syntaxInfo = null;
+            return false;
+        }
+
+        static bool MightBeRelevantType(ITypeSymbol? typeSymbol)
+        {
+            return typeSymbol switch
             {
-                var typeDisplayString = model.ToDisplayString();
-                var processing = new Queue<INamedTypeSymbol>();
-                processing.Enqueue(model);
+                null => false,
+                { ContainingNamespace: { } ns } when IsRelevantNamespace(ns) => true,
+                { BaseType: { } baseType } => MightBeRelevantType(baseType),
+                { AllInterfaces: { Length: > 0 } interfaces } => interfaces.Any(i => IsRelevantNamespace(i.ContainingNamespace)),
+                _ => false
+            };
 
-                var current = model.BaseType;
+            static bool IsRelevantNamespace(INamespaceSymbol namespaceSymbol)
+            {
+                var ns = namespaceSymbol.ToDisplayString();
+                return ns.StartsWith("HotChocolate") || ns.StartsWith("GreenDonut");
+            }
+        }
 
-                while (current is not null)
+        var model = context.SemanticModel.GetDeclaredSymbol(possibleType);
+
+        foreach (var baseTypeSymbol in baseTypeSymbols)
+        {
+            if (WellKnownTypes.TypeClass
+                .SelectMany(n => context.SemanticModel.Compilation.GetTypesByMetadataName(n))
+                .Any(t => context.SemanticModel.Compilation.HasImplicitConversion(baseTypeSymbol, t)))
+            {
+                if (model is { IsAbstract: false, })
                 {
-                    processing.Enqueue(current);
-
-                    var displayString = current.ToDisplayString();
-
-                    if (displayString.Equals(WellKnownTypes.SystemObject, StringComparison.Ordinal))
-                    {
-                        break;
-                    }
-
-                    if (WellKnownTypes.TypeClass.Contains(displayString))
-                    {
-                        syntaxInfo = new TypeInfo(typeDisplayString);
-                        return true;
-                    }
-
-                    if (WellKnownTypes.TypeExtensionClass.Contains(displayString))
-                    {
-                        syntaxInfo = new TypeExtensionInfo(typeDisplayString, false);
-                        return true;
-                    }
-
-                    current = current.BaseType;
+                    var typeDisplayString = model.ToDisplayString();
+                    syntaxInfo = new TypeInfo(typeDisplayString);
+                    return true;
                 }
+            }
+        }
 
-                while (processing.Count > 0)
+        foreach (var baseTypeSymbol in baseTypeSymbols)
+        {
+            if (WellKnownTypes.TypeExtensionClass
+                .SelectMany(n => context.SemanticModel.Compilation.GetTypesByMetadataName(n))
+                .Any(t => context.SemanticModel.Compilation.HasImplicitConversion(baseTypeSymbol, t)))
+            {
+                if (model is { IsAbstract: false, })
                 {
-                    current = processing.Dequeue();
+                    var typeDisplayString = model.ToDisplayString();
+                    syntaxInfo = new TypeExtensionInfo(typeDisplayString, false);
+                    return true;
+                }
+            }
+        }
 
-                    var displayString = current.ToDisplayString();
-
-                    if (displayString.Equals(WellKnownTypes.DataLoader, StringComparison.Ordinal))
-                    {
-                        syntaxInfo =  new RegisterDataLoaderInfo(typeDisplayString);
-                        return true;
-                    }
-
-                    foreach (var interfaceType in current.Interfaces)
-                    {
-                        processing.Enqueue(interfaceType);
-                    }
+        foreach (var baseTypeSymbol in baseTypeSymbols)
+        {
+            if (context.SemanticModel.Compilation
+                .GetTypesByMetadataName(WellKnownTypes.DataLoader)
+                .Any(t => context.SemanticModel.Compilation.HasImplicitConversion(baseTypeSymbol, t)))
+            {
+                if (model is { IsAbstract: false, })
+                {
+                    var typeDisplayString = model.ToDisplayString();
+                    syntaxInfo = new RegisterDataLoaderInfo(typeDisplayString);
+                    return true;
                 }
             }
         }
